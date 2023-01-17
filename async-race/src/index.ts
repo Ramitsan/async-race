336079ab1b6a7cf7e2f319d792c36e8bed17a35a
Ramitsan <ramitsan@gmail.com>
2023-01-17 20:40:49 +0300

    feat: add base requests

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+import  React  from "react";
+import * as ReactDOM from "react-dom/client";
+import { App } from './app';
+
+const rootDiv = document.getElementById('root');
+if (rootDiv === null || !(rootDiv instanceof HTMLElement)) {
+  throw new Error("error");
+}
+const root = ReactDOM.createRoot(rootDiv);
+root.render(React.createElement(App));
