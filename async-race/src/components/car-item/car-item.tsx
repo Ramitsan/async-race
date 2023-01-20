739220b38df8835df109d67056691924fbd7d705
Ramitsan <ramitsan@gmail.com>
2023-01-20 17:22:59 +0300

    fix: styles of car-item component

--- async-race/src/components/car-item/car-item.tsx
+++ async-race/src/components/car-item/car-item.tsx
@@ -7,17 +7,21 @@ export default function CarItem({ data }: { data: ICar }) {
   return (
     <div className="car-item">
       <div className="car-item__wrapper">
-        <span className="car-item__name">{data.name}</span>
-        <div className="car-item__buttons">
-          <div className="car-item__button-wrapper">
-            <button className="btn car-item__btn car-item__btn--edit">Edit</button>
-            <button className="btn car-item__btn car-item__btn--remove">Remove</button>
+        <div className="car-item__top">
+         
+          <div className="car-item__buttons">
+            <div className="car-item__button-wrapper">
+              <button className="btn car-item__btn car-item__btn--edit">Edit</button>
+              <button className="btn car-item__btn car-item__btn--remove">Remove</button>
+            </div>
+
+            <div className="car-item__button-wrapper">
+              <button className="btn car-item__btn car-item__btn--start">Start</button>
+              <button className="btn car-item__btn car-item__btn--stop">Stop</button>
+            </div>
           </div>
 
-          <div className="car-item__button-wrapper">
-            <button className="btn car-item__btn car-item__btn--start">Start</button>
-            <button className="btn car-item__btn car-item__btn--stop">Stop</button>
-          </div>
+          <p className="car-item__name">{data.name}</p>
         </div>
 
         <div className="car-item__track">
